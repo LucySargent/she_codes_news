e31727357a0474ab040bdcd564740a281b600c65
Lucy <lucysargent@y7mail.com>
2021-08-29 18:29:31 +1000

    update form.py

--- she_codes_news/news/forms.py
+++ she_codes_news/news/forms.py
@@ -8,7 +8,22 @@ class StoryForm(ModelForm):
         model = NewsStory
         #list of fields to add to form - basically the fields from the NewStory model
         fields = ['title', 'pub_date', 'content']
-        widgets = {'pub_date': forms.DateInput(format=('%m/%d/%Y'),attrs={'class':'form-control', 'placeholder':'Select a date','type':'date'}),}
-        widgets = {'title': forms.TextInput(attrs={'style': 'border-color: purple;'}),}
+        widgets = {'pub_date': forms.DateInput(format=('%m/%d/%Y'),
+        attrs={'class':'form-control', 'placeholder':'Selecta date','type':'date'
+        }),
+
+        'title': forms.TextInput(attrs={
+                'class': "form-control",
+                'placeholder': 'Story title here...',
+        }),
+
+        'content': forms.Textarea(attrs={
+                'class': "form-control",
+                'placeholder': 'Story content here...'
+                }),
+        }
+
+
+        
         # widgets = {'content': forms.TextInput(attrs={'style': 'border-color: purple;'}),}
         # widgets = {'title': forms.TextInput(attrs={'style': 'margin-left: 30px;'}),}
