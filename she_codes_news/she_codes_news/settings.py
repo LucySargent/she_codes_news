00c5e53091249ee1694381bdaf72640a8b30e630
Lucy <lucysargent@y7mail.com>
2021-09-04 14:26:03 +1000

    Update settings.py

--- she_codes_news/she_codes_news/settings.py
+++ she_codes_news/she_codes_news/settings.py
@@ -89,12 +89,12 @@ WSGI_APPLICATION = 'she_codes_news.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
+DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
-# }
+}
 
 # tell heroku in production to use our postgres database rather than SQLite database
 db_from_env = dj_database_url.config(conn_max_age=500)
